eee87492d59d17a29fdfded5e69ffa3bef36be92
Aisar Nazmi <aisar.nazmiey39@gmail.com>
2022-09-13 00:18:09 +0800

    migrate lara9, scantum api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
-use App\Http\Controllers\LibraryController;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LibraryController;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +22,33 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('library', [LibraryController::class, 'index']);
-Route::post('library/translate', [LibraryController::class, 'translate']);
-Route::post('library/store', [LibraryController::class, 'store']);
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+ 
+    $user = User::where('email', $request->email)->first();
+ 
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+ 
+    return $user->createToken($request->device_name)->plainTextToken;
+});
+
+Route::middleware('auth:sanctum')->get('/user/revoke', function (Request $request) {
+    $user = $request->user();
+    $user->tokens()->delete();
+
+    return 'Tokens are deleted';
+});
+
+Route::controller(LibraryController::class)->group(function () {
+    Route::get('library', 'index');
+    Route::post('library/translate', 'translate');
+    Route::post('library/store', 'store');
+});
