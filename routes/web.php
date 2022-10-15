06810c817beb4682299a14e25c0938f7f81b90d4
Aisar Nazmi <aisar.nazmiey39@gmail.com>
2022-10-16 03:05:23 +0800

    change homepage layout

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Artisan;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/artisan', function (Request $request) {
